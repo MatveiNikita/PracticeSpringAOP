97aba78393c0b17b215700b7ae38f373bc6bbd61
Matvei Nikita <matveinikitaivanovich@gmail.com>
2024-01-30 10:28:55 +0200

    Add a class that is responsible for using aspects

--- src/main/java/com/learnAOP/demo/service/BookService.java
+++ src/main/java/com/learnAOP/demo/service/BookService.java
@@ -24,44 +24,18 @@ public class BookService {
         this.bookRepository = bookRepository;
     }
     public CustomResponse<Book> getAll(){
-        List<Book> books;
-        try {
-            log.info("Try to get all books");
-            books = bookRepository.findAll();
-        }catch (Exception e){
-            log.info(e.getMessage(), e);
-            return new CustomResponse<>(null, CustomStatus.EXCEPTION);
-        }
-        log.info("Got books Successful");
+        List<Book> books = bookRepository.findAll();
         return new CustomResponse<>(books, CustomStatus.SUCCESS);
     }
 
     public CustomResponse<Book> getBookByTitle(String title) {
-        Book book;
-        try {
-            log.info("Try to get book with title - {}", title);
-            book = bookRepository.findBookByTitle(title).orElseThrow();
-        }catch (NoSuchElementException e){
-            log.info(e.getMessage(),e);
-            return new CustomResponse<>(null, CustomStatus.EXCEPTION);
-        }catch (Exception e){
-            log.info(e.getMessage(),e);
-            return new CustomResponse<>(null,CustomStatus.EXCEPTION);
-        }
-        log.info("Got book with title - {}",title);
+        Book book = bookRepository.findBookByTitle(title).orElseThrow();
         return new CustomResponse<>(Stream.of(book).collect(Collectors.toList()), CustomStatus.SUCCESS);
     }
 
     public CustomResponse<Book> addBook(Book book) {
         Book newBook = null;
-        try {
-            log.info("Try to add book {} in our library", book.getTitle());
-            newBook = bookRepository.save(book);
-        }catch (Exception e){
-            log.error(e.getMessage(),e);
-            return new CustomResponse<>(null, CustomStatus.EXCEPTION);
-        }
-        log.info("Book - {} was added", book.getTitle());
+        newBook = bookRepository.save(book);
         return new CustomResponse<>(Stream.of(newBook).collect(Collectors.toList()), CustomStatus.SUCCESS);
     }
 }
